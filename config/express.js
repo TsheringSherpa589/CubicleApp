1753a37662cc647d3e35667e1f8e65bb68e641d5
Tshering Sherpa <rckstr@Tsherings-MacBook-Air.local>
2021-07-31 18:14:57 -0400

    fifirst Commit

--- config/express.js
+++ config/express.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 module.exports = (app) => {
-    
-    //TODO: Setup the view engine
 
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('views engine', 'hbs');
+    
+   
+  
     //TODO: Setup the body parser
 
     //TODO: Setup the static files
--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,22 @@
 // TODO: Require Controllers...
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
 
 module.exports = (app) => {
+    app.get('/', function (req,res) {
+         res.render('index');
+    });
+    app.get('/about', function(req,res) {
+        res.render('Welcome to the about page');
+   });
+   app.get('/create', function (req,res) {
+    res.render('Get request to create page');
+    });
+    app.get('/details/:id', function (req,res) {
+        res.render('details page about selected cude');
+   });
+
     // TODO...
 };
